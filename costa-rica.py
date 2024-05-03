2bda87d9867a1cdb740cd36af088f1732724027c
Quentin Fennessy <qfennessy@gmail.com>
2024-05-03 16:30:41 -0400

    starter

---
+++ costa-rica.py
@@ -0,0 +1,8 @@
+from pinecone import Pinecone, ServerlessSpec
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+PINECONE_API_KEY=os.getenv("PINECONE_API_KEY")
